3ed69b56fbaf7eda41564ae4a90df5b3338832ca
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-04-17 20:06:17 -0500

    Script
    Minor Scripts changes.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const ham = document.querySelector(".hamburguer")
-const img = document.querySelector(".img-close")
-const menu = document.querySelector(".dsk-menu")
+const ham = document.querySelector('.hamburguer');
+const img = document.querySelector('.img-close');
+const menu = document.querySelector('.dsk-menu');
 
-ham.addEventListener("click", () => {
-    menu.classList.toggle("dsk-menu_visible");
-
-})
-img.addEventListener("click", () => {
-    menu.classList.toggle("dsk-menu_visible");
-})
-
+ham.addEventListener('click', () => {
+    menu.classList.toggle('dsk-menu_visible');
+});
+img.addEventListener('click', () => {
+    menu.classList.toggle('dsk-menu_visible');
+});
