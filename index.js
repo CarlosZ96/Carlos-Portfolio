5a13f8ba9d0293f6d4ba2e32ec83f67ea39cab75
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-04-18 13:03:01 -0500

    Update
    Added mobile menu functions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const ham = document.querySelector('.hamburguer');
 const img = document.querySelector('.img-close');
 const menu = document.querySelector('.mbl-menu');
+const mtag1 = document.querySelector('.m-menu-tag1');
+const mtag2 = document.querySelector('.m-menu-tag2');
+const mtag3 = document.querySelector('.m-menu-tag3');
 
 ham.addEventListener('click', () => {
   menu.classList.toggle('mbl-menu_visible');
@@ -8,3 +11,9 @@ ham.addEventListener('click', () => {
 img.addEventListener('click', () => {
   menu.classList.toggle('mbl-menu_visible');
 });
+function hidde() {
+  menu.classList.toggle('mbl-menu_visible');
+}
+mtag1.addEventListener('click', hidde);
+mtag2.addEventListener('click', hidde);
+mtag3.addEventListener('click', hidde);
