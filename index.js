79379a21db5fae53d585025b6061724e71cc68f7
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-04-18 10:56:21 -0500

    Update
    Minor changes.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const img = document.querySelector('.img-close');
 const menu = document.querySelector('.mbl-menu');
 
 ham.addEventListener('click', () => {
-menu.classList.toggle('mbl-menu_visible');
+  menu.classList.toggle('mbl-menu_visible');
 });
 img.addEventListener('click', () => {
-menu.classList.toggle('mbl-menu_visible');
+  menu.classList.toggle('mbl-menu_visible');
 });
