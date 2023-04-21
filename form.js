b082c0d23cd80dbef1afc9249981a76ec8431db5
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-04-21 17:54:29 -0500

    Update
    Creating paragraph for error messages.

--- form.js
+++ form.js
@@ -1,15 +1,14 @@
-const form = document.querySelector(".fields")
-const container = document.querySelector("#email-container")
-const emailInput = form.elements.email
-const messageError = document.createElement("small")
+const form = document.querySelector('.fields');
+const container = document.querySelector('#email-container');
+const emailInput = form.elements.email;
+const EmailmessageError = document.querySelector('.email__error');
+const messageError = document.querySelector('.fields__error');
 
-form.addEventListener("submit",(event)=>{
-    
+form.addEventListener("submit", (event) => {
+
     const emailRegex = (/[A-Z]/)
     if (emailRegex.test(emailInput.value)) {
-        event.preventDefault();        
-        messageError.innerHTML = "Please use lowercase only"
-        container.appendChild(messageError)
-    }
+        event.preventDefault();
 
-})
+    }
+});
