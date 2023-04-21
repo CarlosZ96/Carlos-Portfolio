3640e42a0f13740a566d9f4f75748e3d7a5bede5
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-04-20 22:34:31 -0500

    Popup Window update
    Finish the desktop window.

--- popup.js
+++ popup.js
@@ -1,55 +1,55 @@
 const Mworks = document.getElementById("mwks");
 const detailsc = document.getElementById("popup-c");
 const workse = [
-    {
-        project_tittle: 'Tonic',
-        screen_shot: 'img/Snapshoot1.png',
-        screen_shotdks: '/img/tonic.png',
-        frame: ['Canopy', 'Back End Dev', '2015'],
-        description: 'A daily selection of privately personalized reads; no accounts or sign-ups',
-        tags: ['html', 'Ruby on Rails', 'css', 'javaScript'],
-    },
+  {
+    project_tittle: 'Tonic',
+    screen_shot: 'img/Snapshoot1.png',
+    screen_shotdks: '/img/tonic.png',
+    frame: ['Canopy', 'Back End Dev', '2015'],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups',
+    tags: ['html', 'Ruby on Rails', 'css', 'javaScript'],
+  },
 
-    {
-        project_tittle: 'Multi-Post Stories',
-        screen_shot: 'img/Snapshoot2.png',
-        screen_shotdks: 'img/Snapshoot4.png',
-        frame: ['Canopy', 'Back End Dev', '2015'],
-        framedsk: ['Facebook', 'Back End Dev', '2015'],
-        description: 'A daily selection of privately personalized reads; no accounts or sign-ups',
-        descriptiondsk: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-        tags: ['html', 'Ruby on Rails', 'css', 'javaScript'],
-    },
+  {
+    project_tittle: 'Multi-Post Stories',
+    screen_shot: 'img/Snapshoot2.png',
+    screen_shotdks: 'img/Snapshoot4.png',
+    frame: ['Canopy', 'Back End Dev', '2015'],
+    framedsk: ['Facebook', 'Back End Dev', '2015'],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups',
+    descriptiondsk: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    tags: ['html', 'Ruby on Rails', 'css', 'javaScript'],
+  },
 
-    {
-        project_tittle: 'Tonic',
-        titledsk: 'Facebook 360',
-        screen_shot: 'img/Snapshoot3.png',
-        screen_shotdks: 'img/Snapshoot1.png',
-        frame: ['Canopy', 'Back End Dev', '2015'],
-        framedsk: ['Facebook', 'Back End Dev', '2015'],
-        description: 'A daily selection of privately personalized reads; no accounts or sign-ups',
-        descriptiondsk: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-        tags: ['html', 'Ruby on Rails', 'css', 'javaScript'],
-    },
+  {
+    project_tittle: 'Tonic',
+    titledsk: 'Facebook 360',
+    screen_shot: 'img/Snapshoot3.png',
+    screen_shotdks: 'img/Snapshoot1.png',
+    frame: ['Canopy', 'Back End Dev', '2015'],
+    framedsk: ['Facebook', 'Back End Dev', '2015'],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups',
+    descriptiondsk: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    tags: ['html', 'Ruby on Rails', 'css', 'javaScript'],
+  },
 
-    {
-        project_tittle: 'Multi-Post Stories',
-        titledsk: 'Uber Navigation',
-        screen_shot: 'img/Snapshoot4.png',
-        screen_shotdks: 'img/Snapshoot2.png',
-        frame: ['Canopy', 'Back End Dev', '2015'],
-        framedsk: ['Uber', 'Back End Dev', '2015'],
-        description: 'A daily selection of privately personalized reads; no accounts or sign-ups',
-        tags: ['html', 'Ruby on Rails', 'css', 'javaScript'],
-    },
+  {
+    project_tittle: 'Multi-Post Stories',
+    titledsk: 'Uber Navigation',
+    screen_shot: 'img/Snapshoot4.png',
+    screen_shotdks: 'img/Snapshoot2.png',
+    frame: ['Canopy', 'Back End Dev', '2015'],
+    framedsk: ['Uber', 'Back End Dev', '2015'],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups',
+    tags: ['html', 'Ruby on Rails', 'css', 'javaScript'],
+  },
 ];
 
 for (let i = 0; i < workse.length; i++) {
-    var section = document.createElement('section');
-    Mworks.append(section);
-    section.className = 'Works';
-    section.innerHTML = `
+  var section = document.createElement('section');
+  Mworks.append(section);
+  section.className = 'Works';
+  section.innerHTML = `
     <div class="screen-shot">
     <img src= ${workse[i].screen_shot} alt="project-image" class="screenshot-img">
     </div>
@@ -88,8 +88,8 @@ popup.className = 'details';
 popup.innerHTML = `
 <header class="header-4">
 <div class="top-p">
-<h2 class="project-tittle"></h2>
-<button class="btn-close2" alt="close menu"><img src="img/cros.png" class="img-close2"></button>
+  <h2 class="project-tittle"></h2>
+  <button class="btn-close2" alt="close menu"><img src="img/cros.png" class="img-close2"></button>
 </div>
 <ul class="frame">
   <li class="txt" id="canopy"></li>
@@ -100,25 +100,38 @@ popup.innerHTML = `
 </ul>
 </header>
 <div class="screen-shotp"></div>
+<div class="body-2">
 <div>
-<p id="description-2"></p>
+<p class="descriptionm">
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
+    </p>
+<p class="descriptiondsk">
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  <br>
+  <br>
+  <br>
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.
+    </p>
 </div>
 <div class="buttons">
-<ul id="tags-list">
-  <li  id="h-tag"><a href="#" class="tag" id="html">HTML</a></li>
-  <li  id="r-tag"><a href="#" class="tag" id="ruby">Ruby on rails</a></li>
-  <li  id="c-tag"><a href="#" class="tag" id="css">CSS</a></li>
-  <li  id="j-tag"><a href="#" class="tag" id="JavaScript">JavaScript</a></li>
-  <li  id="b-tag"><a href="#" class="tag" id="JavaScript">Bootstrap</a></li>
-</ul>
-<div class="sbtn">
-<button class="LiveB">
-<img src="img/live.png" alt="live-icon" class="live-img">
-<p id="sbutton"></p>
-</button>
-<button class="gitB">
-<img src="img/git.png" alt="github-icon" class="live-img">
-</button>
+  <ul id="tags-list">
+    <li id="h-tag"><a href="#" class="tag" id="html">HTML</a></li>
+    <li id="c-tag"><a href="#" class="tag" id="css">CSS</a></li>
+    <li id="j-tag"><a href="#" class="tag" id="JavaScript">JavaScript</a></li>
+  </ul>
+  <ul id="tags-list2">
+    <li id="r-tag"><a href="#" class="tag" id="ruby">Ruby on rails</a></li>
+    <li id="b-tag"><a href="#" class="tag" id="ruby">Bootstrap</a></li>
+  </ul>
+  <div class="sbtn">
+    <button class="LiveB">
+      <img src="img/live.png" alt="live-icon" class="live-img">
+      <p id="sbutton"></p>
+    </button>
+    <button class="gitB">
+      <img src="img/git.png" alt="github-icon" class="live-img">
+    </button>
+  </div>
 </div>
 </div>`
 const see = document.querySelector('.NormalB');
