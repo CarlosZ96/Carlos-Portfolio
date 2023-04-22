6424c9f3b47513b78d86b15ed441613dbb8cda0c
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-04-21 21:38:04 -0500

    Update form.js
    Minor script changes.

--- form.js
+++ form.js
@@ -3,9 +3,9 @@ const emailInput = form.elements.email;
 const EmailmessageError = document.querySelector('.email__error');
 
 form.addEventListener('submit', (event) => {
-    const emailRegex = (/[A-Z]/)
-    if (emailRegex.test(emailInput.value)) {
-        event.preventDefault();
-        EmailmessageError.classList.toggle('hidden_email_error');
-    }
+const emailRegex = (/[A-Z]/);
+if (emailRegex.test(emailInput.value)) {
+    event.preventDefault();
+      EmailmessageError.classList.toggle('hidden_email_error');
+}
 });
